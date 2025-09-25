10db303f33ee491de61efc60cc4a86896d78ace5
peter115342 <pmuzslay8@gmail.com>
2025-09-25 08:12:01 +0200

    fix : use official Astro deployment template

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://peter115342.github.io',
-  base: '/TP25_26_webpage',
-  output: 'static',
-  build: {
-    assets: '_astro'
-  }
+  output: 'static'
 });
